camunigeography/panorama-story-map

commit bbe318596e183b4c7920d70f060a7934facfa729

Set icon based on content

marzipano-customised/index.js
@@ -286,6 +286,14 @@
 
   function createInfoHotspotElement(hotspot) {
 
+    // LOCAL CHANGE: Determine the type used for the icon
+    let type = 'info';
+    if (hotspot.text.includes ('Website link')) {type = 'info';}
+    if (hotspot.text.includes ('<img')) {type = 'info';}
+    if (hotspot.text.includes ('<audio')) {type = 'play';}
+    if (hotspot.text.includes ('<video')) {type = 'expand';}
+    if (hotspot.text.includes ('youtube.')) {type = 'expand';}
+    
     // Create wrapper element to hold icon and tooltip.
     var wrapper = document.createElement('div');
     wrapper.classList.add('hotspot');
@@ -299,7 +307,7 @@
     var iconWrapper = document.createElement('div');
     iconWrapper.classList.add('info-hotspot-icon-wrapper');
     var icon = document.createElement('img');
-    icon.src = 'img/info.png';
+    icon.src = 'img/' + type + '.png';
     icon.classList.add('info-hotspot-icon');
     iconWrapper.appendChild(icon);
 

commit a8feb4a3279ebc5ad95af63514b2e3644aa59340

Use icon per type
This also fixes the regression in bbe318596e183b4c7920d70f060a7934facfa729 where the top-right play icon was showing a different icon

marzipano-customised/index.js
@@ -287,12 +287,12 @@
   function createInfoHotspotElement(hotspot) {
 
     // LOCAL CHANGE: Determine the type used for the icon
-    let type = 'info';
-    if (hotspot.text.includes ('Website link')) {type = 'info';}
-    if (hotspot.text.includes ('<img')) {type = 'info';}
-    if (hotspot.text.includes ('<audio')) {type = 'play';}
-    if (hotspot.text.includes ('<video')) {type = 'expand';}
-    if (hotspot.text.includes ('youtube.')) {type = 'expand';}
+    let type = 'custom/info';
+    if (hotspot.text.includes ('Website link')) {type = 'custom/website';}
+    if (hotspot.text.includes ('<img')) {type = 'custom/photo';}
+    if (hotspot.text.includes ('<audio')) {type = 'custom/audio';}
+    if (hotspot.text.includes ('<video')) {type = 'custom/video';}
+    if (hotspot.text.includes ('youtube.')) {type = 'custom/video';}
     
     // Create wrapper element to hold icon and tooltip.
     var wrapper = document.createElement('div');